d3272e904828ae2e5e91317e905a986419a99805
Fernando Solano <fersolano@ex2outcoding.local>
2020-06-13 03:18:11 -0600

    Make item module with express to make the get, create delete and update

---
+++ functions/routes/item.js
@@ -0,0 +1,92 @@
+'use strict'
+
+const express = require('express'),
+    router = express.Router();
+
+// create
+router.post('/create', (req, res) => {
+    (async () => {
+        try {
+            await db.collection('items').doc('/' + req.body.id + '/').create({item: req.body.item});
+            return res.status(200).send();
+        } catch (error) {
+            console.log(error);
+            return res.status(500).send(error);
+        }
+      })();
+  });
+  
+router.get('/hello-world', (req, res) => {
+  return res.status(200).send('Hello World!');
+});
+
+router.get('/read/:item_id', (req, res) => {
+    (async () => {
+        try {
+            const document = db.collection('items').doc(req.params.item_id);
+            let item = await document.get();
+            let response = item.data();
+            return res.status(200).send(response);
+        } catch (error) {
+            console.log(error);
+            return res.status(500).send(error);
+        }
+        })();
+    });
+
+// read all
+router.get('/read', (req, res) => {
+    (async () => {
+        try {
+            let query = db.collection('items');
+            let response = [];
+            await query.get().then(querySnapshot => {
+                let docs = querySnapshot.docs;
+                for (let doc of docs) {
+                    const selectedItem = {
+                        id: doc.id,
+                        item: doc.data().item
+                    };
+                    response.push(selectedItem);
+                }
+                return response;
+            });
+            return res.status(200).send(response);
+        } catch (error) {
+            console.log(error);
+            return res.status(500).send(error);
+        }
+        })();
+    });
+
+// update
+router.put('/update/:item_id', (req, res) => {
+(async () => {
+    try {
+        const document = db.collection('items').doc(req.params.item_id);
+        await document.update({
+            item: req.body.item
+        });
+        return res.status(200).send();
+    } catch (error) {
+        console.log(error);
+        return res.status(500).send(error);
+    }
+    })();
+});
+
+// delete
+router.delete('/delete/:item_id', (req, res) => {
+(async () => {
+    try {
+        const document = db.collection('items').doc(req.params.item_id);
+        await document.delete();
+        return res.status(200).send();
+    } catch (error) {
+        console.log(error);
+        return res.status(500).send(error);
+    }
+    })();
+});
+
+module.exports = router;
