1f9de4fde7b6ab65d1b9b2ed830e249ada703a3d
Andrés Bonilla <abonilla@ucenfotec.ac.cr>
2020-07-05 12:26:17 -0600

    Improved sintaxis of all methods on Careers.js route

--- functions/routes/careers.js
+++ functions/routes/careers.js
@@ -6,82 +6,62 @@ const router = express.Router();
 const admin = require('firebase-admin');
 const db = admin.firestore();
 
-router.post('/careers/create', (req, res) =>{
-    (async ()=>{
-        try{
-            await db.collection('careers').doc('/' + req.body.code + '/')
-            .create({ name: req.body.name});
-            return res.status(200).send({message: 'Career created'});
-        }catch(error){
-            console.log(error);
-            return res.status(500).send(error);
-        }
-    })();
-});
-
-router.get('/careers/read/:career_code', (req,res)=>{
-    (async () =>{
-        try{
-            const document = db.collection('careers').doc(req.params.career_code);
-            const career = await document.get();
-            const response = career.data();
-            return res.status(200).send(response);
-        }catch(error){
-            return res.status(500).send(error);
-        }
-    })();
-});
-
-router.get('/careers/read', (req,res) =>{
+router.post('/careers/create', (req, res) => {
     (async () => {
-        try{
-            const collection = db.collection('careers');
-            const response = [];
-            await collection.get().then(querySnapshot =>{
-                const docs = querySnapshot.docs;
-                for(let doc of docs){
-                    const newCareer = {
+
+        await db.collection('careers').doc(req.body.code)
+            .create({ name: req.body.name })
+            .then(() => res.status(200).send({ message: 'Career created' }))
+            .catch((error) => res.status(500).send(error))
+    })();
+});
+
+router.get('/careers/read/:career_code', (req, res) => {
+    (async () => {
+        const document = db.collection('careers').doc(req.params.career_code);
+        await document.get()
+            .then((querySnapshot) => res.status(200).send(querySnapshot.data()))
+            .catch((error) => res.status(500).send(error))
+    })();
+});
+
+router.get('/careers/read', (req, res) => {
+    (async () => {
+        const collection = db.collection('careers');
+        const response = [];
+        await collection.get()
+            .then(querySnapshot => {
+                querySnapshot.docs.forEach(doc => {
+                    const data = {
                         id: doc.id,
-                        code: doc.data().code,
-                        name: doc.data().name
-                    };
-                    response.push(newCareer);
-                }
-                return response;
-            });
-            return res.status(200).send(response);
-        }catch(error){
-            return res.status(500).send(error);
-        }
+                        ...doc.data()
+                    }
+                    response.push(data);
+                })
+            })
+            .then(() => res.status(200).send(response))
+            .catch((error) => res.status(500).send(error))
     })();
 });
 
 router.put('/careers/update/:career_code', (req, res) => {
     (async () => {
-        try {
-            const document = db.collection('careers').doc(req.params.career_code);
-            await document.update({
-                name: req.body.name
-            });
-            return res.status(200).send({message: 'Career updated'});
-        } catch (error) {
-            console.log(error);
-            return res.status(500).send(error);
-        }
+        const document = db.collection('careers').doc(req.params.career_code);
+        await document.update({
+            name: req.body.name
+        })
+            .then(() => res.status(200).send({ message: 'Career updated' }))
+            .catch((error) => res.status(500).send(error))
     })();
 });
 
 
 router.delete('/careers/delete/:career_code', (req, res) => {
     (async () => {
-        try {
-            const document = db.collection('careers').doc(req.params.career_code);
-            await document.delete();
-            return res.status(200).send({message: 'Career deleted'});
-        } catch (error) {
-            console.log(error);
-            return res.status(500).send(error);
-        }
+        const document = db.collection('careers').doc(req.params.career_code);
+        await document.delete()
+            .then(() => res.status(200).send({ message: 'Career deleted' }))
+            .catch((error) => res.status(500).send(error))
     })();
 });
 
