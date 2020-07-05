05dfa6818487303f8cccb0b330f82e50aceb7c37
Andrés Bonilla <abonilla@ucenfotec.ac.cr>
2020-07-05 12:36:33 -0600

    Code updated for course and students entities

--- functions/routes/courses.js
+++ functions/routes/courses.js
@@ -8,90 +8,66 @@ const db = admin.firestore();
 
 router.post('/courses/create', (req, res) => {
     (async () => {
-        try {
-            await db.collection('courses').doc('/' + req.body.code + '/')
-                .create(
-                    {
-                        name: req.body.name,
-                        credits: req.body.credits,
-                        cost: req.body.cost
-                    }
-                );
-            return res.status(200).send({ message: 'Course created' });
-        } catch (error) {
-            console.log(error);
-            return res.status(500).send(error);
-        }
+        await db.collection('courses').doc(req.body.code)
+            .create(
+                {
+                    name: req.body.name,
+                    credits: req.body.credits,
+                    cost: req.body.cost
+                }
+            )
+            .then(() => res.status(200).send({ message: 'Course created' }))
+            .catch((error) => res.status(500).send(error))
     })();
 });
 
 router.get('/courses/read/:course_id', (req, res) => {
     (async () => {
-        try {
-            const document = db.collection('courses').doc(req.params.course_id);
-            const course = await document.get();
-            const response = course.data();
-            return res.status(200).send(response);
-        } catch (error) {
-            return res.status(500).send(error);
-        }
+        const document = db.collection('courses').doc(req.params.course_id);
+        await document.get()
+            .then((querySnapshot) => res.status(200).send(querySnapshot.data()))
+            .catch((error) => res.status(500).send(error))
     })();
 });
 
 router.get('/courses/read', (req, res) => {
     (async () => {
-        try {
-            const collection = db.collection('courses');
-            const response = [];
-            await collection.get().then(querySnapshot => {
-                const docs = querySnapshot.docs;
-                for (let doc of docs) {
-                    const newCourse = {
+        const collection = db.collection('courses');
+        const response = [];
+        await collection.get()
+            .then(querySnapshot => {
+                querySnapshot.docs.forEach(doc => {
+                    const data = {
                         id: doc.id,
-                        code: doc.data().code,
-                        name: doc.data().name,
-                        credits: doc.data().credits,
-                        cost: doc.data().cost
-                    };
-                    response.push(newCourse);
-                }
-                return response;
-            });
-            return res.status(200).send(response);
-        } catch (error) {
-            return res.status(500).send(error);
-        }
+                        ...doc.data()
+                    }
+                    response.push(data);
+                })
+            })
+            .then(() => res.status(200).send(response))
+            .catch((error) => res.status(500).send(error))
     })();
 });
 
 router.put('/courses/update/:course_id', (req, res) => {
     (async () => {
-        try {
-            const document = db.collection('courses').doc(req.params.course_id);
-            await document.update({
-                name: req.body.name,
-                credits: req.body.credits,
-                cost: req.body.cost
-            });
-            return res.status(200).send({ message: 'Course updated' });
-        } catch (error) {
-            console.log(error);
-            return res.status(500).send(error);
-        }
+        const document = db.collection('courses').doc(req.params.course_id);
+        await document.update({
+            name: req.body.name,
+            credits: req.body.credits,
+            cost: req.body.cost
+        })
+            .then(() => res.status(200).send({ message: 'Course updated' }))
+            .catch((error) => res.status(500).send(error))
     })();
 });
 
-// delete
 router.delete('/courses/delete/:course_id', (req, res) => {
     (async () => {
-        try {
-            const document = db.collection('courses').doc(req.params.course_id);
-            await document.delete();
-            return res.status(200).send({ message: 'Course deleted' });
-        } catch (error) {
-            console.log(error);
-            return res.status(500).send(error);
-        }
+        const document = db.collection('courses').doc(req.params.course_id);
+        await document.delete()
+            .then(() => res.status(200).send({ message: 'Course deleted' }))
+            .catch((error) => res.status(500).send(error))
     })();
 });
 
--- functions/routes/students.js
+++ functions/routes/students.js
@@ -8,96 +8,71 @@ const db = admin.firestore();
 
 router.post('/students/create', (req, res) => {
     (async () => {
-        try {
-            await db.collection('students').doc('/' + req.body.id + '/')
-                .create(
-                    {
-                        college_id: req.body.college_id,
-                        name: req.body.name,
-                        first_last_name: req.body.first_last_name,
-                        second_last_name: req.body.second_last_name,
-                        start_date: req.body.start_date
-                    }
-                );
-            return res.status(200).send({ message: 'Student created' });
-        } catch (error) {
-            console.log(error);
-            return res.status(500).send(error);
-        }
+        await db.collection('students').doc(req.body.id)
+            .create(
+                {
+                    college_id: req.body.college_id,
+                    name: req.body.name,
+                    first_last_name: req.body.first_last_name,
+                    second_last_name: req.body.second_last_name,
+                    start_date: req.body.start_date
+                }
+            )
+            .then(() => res.status(200).send({ message: 'Student created' }))
+            .catch((error) => res.status(500).send(error))
     })();
 });
 
 router.get('/students/read/:student_id', (req, res) => {
     (async () => {
-        try {
-            const document = db.collection('students').doc(req.params.student_id);
-            const student = await document.get();
-            const response = student.data();
-            return res.status(200).send(response);
-        } catch (error) {
-            return res.status(500).send(error);
-        }
+        const document = db.collection('students').doc(req.params.student_id);
+        await document.get()
+            .then((querySnapshot) => res.status(200).send(querySnapshot.data()))
+            .catch((error) => res.status(500).send(error))
     })();
 });
 
 router.get('/students/read', (req, res) => {
     (async () => {
-        try {
-            const collection = db.collection('students');
-            const response = [];
-            await collection.get().then(querySnapshot => {
-                const docs = querySnapshot.docs;
-                for (let doc of docs) {
-                    const newStudent = {
+        const collection = db.collection('students');
+        const response = [];
+        await collection.get()
+            .then(querySnapshot => {
+                querySnapshot.docs.forEach(doc => {
+                    const data = {
                         id: doc.id,
-                        college_id: doc.data().college_id,
-                        name: doc.data().name,
-                        first_last_name: doc.data().first_last_name,
-                        second_last_name: doc.data().second_last_name,
-                        start_date: doc.data().start_date
-                    };
-                    response.push(newStudent);
-                }
-                return response;
-            });
-            return res.status(200).send(response);
-        } catch (error) {
-            return res.status(500).send(error);
-        }
+                        ...doc.data()
+                    }
+                    response.push(data);
+                })
+            })
+            .then(() => res.status(200).send(response))
+            .catch((error) => res.status(500).send(error))
     })();
 });
 
 router.put('/students/update/:student_id', (req, res) => {
     (async () => {
-        try {
-            const document = db.collection('students').doc(req.params.student_id);
-            await document.update({
-                college_id: req.body.college_id,
-                name: req.body.name,
-                first_last_name: req.body.first_last_name,
-                second_last_name: req.body.second_last_name,
-                start_date: req.body.start_date
-            });
-            return res.status(200).send({ message: 'Student updated' });
-        } catch (error) {
-            console.log(error);
-            return res.status(500).send(error);
-        }
+        const document = db.collection('students').doc(req.params.student_id);
+        await document.update({
+            college_id: req.body.college_id,
+            name: req.body.name,
+            first_last_name: req.body.first_last_name,
+            second_last_name: req.body.second_last_name,
+            start_date: req.body.start_date
+        })
+            .then(() => res.status(200).send({ message: 'Student updated' }))
+            .catch((error) => res.status(500).send(error))
     })();
 });
 
 router.delete('/students/delete/:student_id', (req, res) => {
     (async () => {
-        try {
-            const document = db.collection('students').doc(req.params.student_id);
-            await document.delete();
-            return res.status(200).send({ message: 'Student deleted' });
-        } catch (error) {
-            console.log(error);
-            return res.status(500).send(error);
-        }
+        const document = db.collection('students').doc(req.params.student_id);
+        await document.delete()
+            .then(() => res.status(200).send({ message: 'Student deleted' }))
+            .catch((error) => res.status(500).send(error))
     })();
 });
 
 module.exports = router;
-
