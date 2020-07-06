6e47b79e0ee849bf64845ebabb85cf3a03c55909
FernandoSolano20 <48603523+FernandoSolano20@users.noreply.github.com>
2020-07-06 02:48:04 -0600

    Update Jenkinsfile.groovy

--- Jenkinsfile.groovy
+++ Jenkinsfile.groovy
@@ -1,6 +1,14 @@
-import hudson.Util;
-import groovy.json.JsonOutput;
-
 node {
-    powershell 'Write-Output "Hello, World!"'
+   stage 'test'
+   def whatThe = someFunc('textToFunc')
+   def whatThe2 = someFunc2('textToFunc2')
+}
+
+def someFunc(String text){
+    echo text
+    text
+}
+def someFunc2(String text2){
+    echo text2
+    text2
 }
