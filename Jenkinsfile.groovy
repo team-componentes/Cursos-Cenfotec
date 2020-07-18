f92a53e58acf648fe22b0923f54e016fcb8ff8a2
Fernando Solano <fersolano@ex2outcoding.local>
2020-07-18 03:24:03 -0600

    Java version

--- Jenkinsfile.groovy
+++ Jenkinsfile.groovy
@@ -1,13 +1,6 @@
 pipeline {
     agent none 
     stages {
-        stage('Example Build') {
-            agent { docker 'maven:3-alpine' } 
-            steps {
-                echo 'Hello, Maven'
-                sh 'mvn --version'
-            }
-        }
         stage('Example Test') {
             agent { docker 'openjdk:8-jre' } 
             steps {
