6fc1d14cad6e0f32b442741282eacdd514bff9ff
Fernando Solano <fersolano@ex2outcoding.local>
2020-07-18 02:51:55 -0600

    jenkins test

--- functions/index.js
+++ functions/index.js
@@ -9,7 +9,7 @@ const serviceAccount = require("./permissions.json");
 
 const itemRoute = require('./routes/item');
 
-//test one it jaja
+//test one it
 admin.initializeApp({
   credential: admin.credential.cert(serviceAccount),
   databaseURL: "https://cursos-cenfotec.firebaseio.com"
