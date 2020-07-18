a77a7810834b9d31c27c23355735853b84bc3374
Fernando Solano <fersolano@ex2outcoding.local>
2020-07-18 03:25:07 -0600

    One more time

--- Jenkinsfile.groovy
+++ Jenkinsfile.groovy
@@ -1,8 +1,13 @@
 pipeline {
     agent none 
     stages {
+        stage('Example Build') {
+            steps {
+                echo 'Hello, Maven'
+                sh 'mvn --version'
+            }
+        }
         stage('Example Test') {
-            agent { docker 'openjdk:8-jre' } 
             steps {
                 echo 'Hello, JDK'
                 sh 'java -version'
