7a59d1df8595e4309c992fda04516af2d7fccffd
Fernando Solano <fersolano@ex2outcoding.local>
2020-07-18 02:39:05 -0600

    Testing again

--- functions/index.js
+++ functions/index.js
@@ -9,7 +9,7 @@ const serviceAccount = require("./permissions.json");
 
 const itemRoute = require('./routes/item');
 
-//test one it 
+//test one it jaja
 admin.initializeApp({
   credential: admin.credential.cert(serviceAccount),
   databaseURL: "https://cursos-cenfotec.firebaseio.com"
